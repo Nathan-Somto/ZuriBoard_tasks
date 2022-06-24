84d1601a402af0677edae6987096af66056bebb3
wildcatmidnight883 <92867046+wildcatmidnight883@users.noreply.github.com>
2022-06-24 12:38:23 +0100

    Add files via upload

---
+++ basic_calculator.js
@@ -0,0 +1,6 @@
+let a =5;
+let b =3;
+console.log(`the addition is ${a+b} `);
+console.log(`the subtraction is ${a-b} `);
+console.log(`the multiplication is ${a*b} `);
+console.log(`the division is ${a/b} `);
---
+++ intro_to_js.js
@@ -0,0 +1,4 @@
+var name="Mkparu Somtochi Elnathan";
+var height= "5 ft 8 inches";
+var country="Nigeria";
+console.log(`hello my name is ${name} \n i am ${height} tall and i am from ${country}`);
